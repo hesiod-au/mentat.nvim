c753508463adce15497cc3f4573599224c1c5d02
hesiod-au <hesiod-au@cloudrefactor.engineer>
2024-02-09 16:37:35 +1100

    test 6

--- lua/mentat/init.lua
+++ lua/mentat/init.lua
@@ -85,8 +85,8 @@ M.init_keys = function()
         -- vim.api.nvim_set_keymap("n", Config.options.mentat_keybind, "<Cmd>lua require('mentat').open_terminal_mentat_all_files(" .. Config.options.mentat_start_width .. "," .. "'" .. Config.options.mentat_pre_cmd .. "'" .. ")<CR>", {silent = true})
         -- vim.api.nvim_set_keymap("v", Config.options.mentat_keybind, "<Cmd>lua require('mentat').open_terminal_mentat_selected_only(" .. Config.options.mentat_start_width .. "," .. "'" .. Config.options.mentat_pre_cmd .. "'" .. ")<CR>", {silent = true})
 
-        -- vim.api.nvim_set_keymap("n", Config.options.mentat_keybind, "<Cmd>lua require('mentat').open_terminal_mentat_all_files(" .. Config.options.mentat_start_width .. ")<CR>", {silent = true})
-        -- vim.api.nvim_set_keymap("v", Config.options.mentat_keybind, "<Cmd>lua require('mentat').open_terminal_mentat_selected_only(" .. Config.options.mentat_start_width .. ")<CR>", {silent = true})
+        vim.api.nvim_set_keymap("n", Config.options.mentat_keybind, "<Cmd>lua require('mentat').open_terminal_mentat_all_files(" .. Config.options.mentat_start_width .. ")<CR>", {silent = true})
+        vim.api.nvim_set_keymap("v", Config.options.mentat_keybind, "<Cmd>lua require('mentat').open_terminal_mentat_selected_only(" .. Config.options.mentat_start_width .. ")<CR>", {silent = true})
     end
 end
 
